4ca55e83047bda562c32ddb10680a62957ab38cf
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-03-07 08:49:48 +0700

    Create Wish Added

--- PRN221_Secondhand/Controllers/WishController.cs
+++ PRN221_Secondhand/Controllers/WishController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repository.Models;
 using Repository.Repository;
+using System;
 using System.Linq;
 
 namespace PRN221_Secondhand.Controllers
@@ -13,6 +15,23 @@ namespace PRN221_Secondhand.Controllers
         {
             var _listWish = wishRepo.GetAll().Include(p => p.Post.Category).Include(p => p.Post).Where(p => p.Status != 0);
             return View(_listWish);
+        }
+        public IActionResult Create(string id, HttpContext context)
+        {
+            var session = context.Session;
+            if (session != null)
+            {
+                var userid = session.GetString("user_id");
+                if (userid != null)
+                {
+                    Wish wish = new Wish();
+                    wish.Id = new Guid().ToString();
+                    wish.Created = DateTime.Now;
+                    wish.UserId = userid;
+                    wish.Status = 1;
+                }
+            }
+            return RedirectToAction("Index", "Post");
         }
         public IActionResult Remove(string id)
         {
