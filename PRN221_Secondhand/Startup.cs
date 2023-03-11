13c98b99c5318aeb2fed6a4a4198a1dddd1df618
HaiHP <Haihpse150218@gmail.com>
2023-03-11 11:57:45 +0700

    Setting connection DB

--- PRN221_Secondhand/Startup.cs
+++ PRN221_Secondhand/Startup.cs
@@ -24,6 +24,10 @@ namespace PRN221_Secondhand
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<SecondhandContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("Secondhand")
+                ));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
