376d32fb9e270210d253292bf19551116f2a0779
namnvdse150231 <namnvdse150231@fpt.edu.vn>
2023-03-17 01:52:00 +0700

    'fixlayout'

--- PRN221_Secondhand/Controllers/PostController.cs
+++ PRN221_Secondhand/Controllers/PostController.cs
@@ -11,7 +11,7 @@ namespace PRN221_Secondhand.Controllers
         public IActionResult Index(string id)
         {
             PostRepository postRepository = new PostRepository();
-            Post p = (Post)postRepository.GetAll().Include(t => t.Category).Where(a=>a.Id.Equals(id)).FirstOrDefault()  ;
+            Post p = (Post)postRepository.GetAll().Include(t => t.Category).Where(a=>a.Id.Equals(id)&&a.Status!=0).FirstOrDefault()  ;
             if(p!= null && p.Status == 1)
                 return View(p);
             return View();
